0b80ac7c2ba11c53860a232400ee09ea692b3991
Tom Gobich <fallofmars@gmail.com>
2016-10-25 14:35:09 -0400

    boat listing API call and element listing working

--- dist/js/boats.js
+++ dist/js/boats.js
@@ -2,8 +2,21 @@
 
 $(document).ready(function () {
 
+	// --------------------------------------------------
+	// Build boat listing content
+	// --------------------------------------------------
 	var Boats = function () {
 
+		// --------------------------------------------------
+		// Initialize Module
+		// --------------------------------------------------
+		function init() {
+			getBoatListing();
+		}
+
+		// --------------------------------------------------
+		// Make API call to get back boat data
+		// --------------------------------------------------
 		function getBoatListing() {
 			$.ajax({
 				url: 'https://tiyagencyweek.herokuapp.com/boats',
@@ -13,21 +26,31 @@ $(document).ready(function () {
 			});
 		}
 
+		// --------------------------------------------------
+		// Build HTML Element for boat listings
+		// --------------------------------------------------
 		function displayBoats(data) {
+			var $boatListing = $('#boatListing');
 			var boats = data.boats;
 			var boatElement = '';
 
 			boats.forEach(function (boat) {
-				boatElement += '<div class="col-md-12">\n\t\t\t\t\t\t<div class="boat">\n\t\t\t\t\t\t\t<img class="img-responsive boat-image" src="http://media.channelblade.com/boat_graphics/dealers/7061/digi56284260_l.jpg" alt="boat">\n\t\t\t\t\t\t\t<div class="boat-details">\n\t\t\t\t\t\t\t\t<h5 class="title">Name:</h5>\n\t\t\t\t\t\t\t\t<h3 class="name">2010 Sun Tracker</h3>\n\t\t\t\t\t\t\t\t<h5 class="title">Price:</h5>\n\t\t\t\t\t\t\t\t<h4 class="price">$37,188</h4>\n\t\t\t\t\t\t\t\t<h5 class="title">Description:</h5>\n\t\t\t\t\t\t\t\t<p class="description">\n\t\t\t\t\t\t\t\t\tOne of the best boats there is. Make sure that you get this boat today. \n\t\t\t\t\t\t\t\t\tIts on sale. Includes everything you need!\n\t\t\t\t\t\t\t\t</p>\n\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t</div>\n\t\t\t\t\t</div>';
+				var price = accounting.formatMoney(boat.price);
+
+				boatElement += '<div class="col-md-12">\n\t\t\t\t\t\t<div class="boat">\n\t\t\t\t\t\t\t<img class="img-responsive boat-image" src="' + boat.picture + '" alt="' + boat.name + '">\n\t\t\t\t\t\t\t<div class="boat-details">\n\t\t\t\t\t\t\t\t<h5 class="title">Name:</h5>\n\t\t\t\t\t\t\t\t<h3 class="name">' + boat.name + '</h3>\n\t\t\t\t\t\t\t\t<h5 class="title">Price:</h5>\n\t\t\t\t\t\t\t\t<h4 class="price">' + price + '</h4>\n\t\t\t\t\t\t\t\t<h5 class="title">Description:</h5>\n\t\t\t\t\t\t\t\t<p class="description">' + boat.description + '</p>\n\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t</div>\n\t\t\t\t\t</div>';
 			});
 
-			$('#boatListing').html(boatElement);
+			$boatListing.html(boatElement);
 		}
 
+		// --------------------------------------------------
+		// Make public the following:
+		// --------------------------------------------------
 		return {
-			getBoatListing: getBoatListing
+			init: init
 		};
 	}();
 
-	Boats.getBoatListing();
+	// Initialize boat listing
+	Boats.init();
 });
--- js/boats.js
+++ js/boats.js
@@ -1,54 +1,81 @@
 $(document).ready(function()
 {
 
+	// --------------------------------------------------
+	// Build boat listing content
+	// --------------------------------------------------
 	var Boats = (function()
 	{
 
+		// --------------------------------------------------
+		// Initialize Module
+		// --------------------------------------------------
+		function init()
+		{
+			getBoatListing();
+		}
+
+
+
+		// --------------------------------------------------
+		// Make API call to get back boat data
+		// --------------------------------------------------
 		function getBoatListing()
 		{
 			$.ajax({
 				url: 'https://tiyagencyweek.herokuapp.com/boats',
 				success: (resp) => {
-					displayBoats(resp)
+					displayBoats(resp);
 				}
 			});
 		}
 
+
+
+		// --------------------------------------------------
+		// Build HTML Element for boat listings
+		// --------------------------------------------------
 		function displayBoats(data)
 		{
+			let $boatListing = $('#boatListing');
 			let boats = data.boats;
 			let boatElement = '';
 
 			boats.forEach(function(boat)
 			{
+				let price = accounting.formatMoney(boat.price);
+
 				boatElement +=
 					`<div class="col-md-12">
 						<div class="boat">
-							<img class="img-responsive boat-image" src="http://media.channelblade.com/boat_graphics/dealers/7061/digi56284260_l.jpg" alt="boat">
+							<img class="img-responsive boat-image" src="${boat.picture}" alt="${boat.name}">
 							<div class="boat-details">
 								<h5 class="title">Name:</h5>
-								<h3 class="name">2010 Sun Tracker</h3>
+								<h3 class="name">${boat.name}</h3>
 								<h5 class="title">Price:</h5>
-								<h4 class="price">$37,188</h4>
+								<h4 class="price">${price}</h4>
 								<h5 class="title">Description:</h5>
-								<p class="description">
-									One of the best boats there is. Make sure that you get this boat today. 
-									Its on sale. Includes everything you need!
-								</p>
+								<p class="description">${boat.description}</p>
 							</div>
 						</div>
 					</div>`;
 			});
 
-			$('#boatListing').html(boatElement);
+			$boatListing.html(boatElement);
 		}
 
+
+
+		// --------------------------------------------------
+		// Make public the following:
+		// --------------------------------------------------
 		return {
-			getBoatListing: getBoatListing
+			init: init
 		}
 
 	})();
 
-	Boats.getBoatListing();
+	// Initialize boat listing
+	Boats.init();
 
 });
