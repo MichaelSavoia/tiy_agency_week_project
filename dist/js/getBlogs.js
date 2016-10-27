4a8e7153950d5e2f5b29e6eedcd154eda698e977
Kyle Pfahler <pfahlerk1@gmail.com>
2016-10-27 14:23:27 -0400

    added basic blog post js stuff

--- dist/js/getBlogs.js
+++ dist/js/getBlogs.js
@@ -9,6 +9,11 @@ $(document).ready(function () {
 		headers: { "X_CSRF_TOKEN": localStorage.getItem('token') },
 		success: function success(blogPosts) {
 			console.log(blogPosts);
+			var $blogContainer = $('#blogContainer');
+			blogPosts.blogs.forEach(function (blogPost) {
+				var blogPostTime = blogPost.posted;
+				$blogContainer.append('\n\t\t\t\t\t<div class="row" id="blogPost">\n\t\t\t\t\t\t<div class="col-xs-12 contentRow">\n\t\t\t\t\t\t\t<div class="blogPost">\n\t\t\t\t\t\t\t\t<div class="blogContent">\n\t\t\t\t\t\t\t\t\t<h2 class="blogTitle">' + blogPost.title + '</h2>\n\t\t\t\t\t\t\t\t\t<p class="authorName">By Jake Boyles - Posted on ' + blogPostTime + '</p>\n\t\t\t\t\t\t\t\t\t<article>' + blogPost.description + '</article>\n\t\t\t\t\t\t\t\t\t<p class="readMore">Read More</p>\n\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t<div>\t\n\t\t\t\t\t</div\n\t\t\t\t\t');
+			});
 		},
 		error: function error(err) {
 			console.log("Error! Message: " + e.responseText);
@@ -18,13 +23,14 @@ $(document).ready(function () {
 		}
 	});
 
-	var $article = $('article');
-	var $articleHtml = $article.html().toString();
-	var $articleLength = $articleHtml.length;
-	console.log($articleHtml.length);
+	// let $article = $(`article`)
+	// let $articleHtml= $article.html().toString()
+	// let $articleLength = $articleHtml.length
+	// console.log($articleHtml.length)
 
-	if ($articleLength > 400) {
-		var newArticle = $articleHtml.substr(0, 400);
-		console.log(newArticle);
-	}
+
+	// if ($articleLength > 400) {
+	// 	let newArticle = $articleHtml.substr(0, 400)
+	// 	console.log(newArticle)
+	// }
 });
--- js/getBlogs.js
+++ js/getBlogs.js
@@ -9,19 +9,37 @@ $(document).ready(function () {
 		headers: {"X_CSRF_TOKEN": localStorage.getItem('token')},
 		success: (blogPosts) => {
 			console.log(blogPosts)
+			let $blogContainer = $('#blogContainer')
+			blogPosts.blogs.forEach(function (blogPost) {
+				let blogPostTime = blogPost.posted
+				$blogContainer.append(`
+					<div class="row" id="blogPost">
+						<div class="col-xs-12 contentRow">
+							<div class="blogPost">
+								<div class="blogContent">
+									<h2 class="blogTitle">${blogPost.title}</h2>
+									<p class="authorName">By Jake Boyles - Posted on ${blogPostTime}</p>
+									<article>${blogPost.description}</article>
+									<p class="readMore">Read More</p>
+								</div>
+							<div>	
+					</div
+					`)
+			})
 		},
 		error: function(err) { console.log("Error! Message: " + e.responseText); },
   		complete: function() { console.log("All done!"); }
 	})
 
-	let $article = $(`article`)
-	let $articleHtml= $article.html().toString()
-	let $articleLength = $articleHtml.length
-	console.log($articleHtml.length)
+	// let $article = $(`article`)
+	// let $articleHtml= $article.html().toString()
+	// let $articleLength = $articleHtml.length
+	// console.log($articleHtml.length)
 
 
-	if ($articleLength > 400) {
-		let newArticle = $articleHtml.substr(0, 400)
-		console.log(newArticle)
-	}
+	// if ($articleLength > 400) {
+	// 	let newArticle = $articleHtml.substr(0, 400)
+	// 	console.log(newArticle)
+	// }
 });
+	
