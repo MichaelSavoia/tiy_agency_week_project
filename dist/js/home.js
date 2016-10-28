a0a5d4e66e2596acc470eea97b3c5feb39348e72
Michael Savoia <MichaelSavoia@Michaels-MacBook-Pro.local>
2016-10-28 12:48:50 -0400

    view recent 3 blog posts/styling

--- dist/js/home.js
+++ dist/js/home.js
@@ -1,22 +1,57 @@
-'use strict';
+"use strict";
 
-$.ajax({
-	url: "https://tiyagencyweek.herokuapp.com/blogs",
-	success: function success(result) {
-		console.log(result);
-		var blogArray = result.blogs;
-		console.log(blogArray);
+$(document).ready(function () {
 
-		$('#blogTitle1').append('\n\t\t\t' + blogArray[0].title + '\n\t\t');
+	var viewBlog = function () {
 
-		$('#blogText1').append('\n\t\t\t' + blogArray[0].text + '\n\t\t');
+		function init() {
+			callBlogAPI();
+		}
 
-		$('#blogTitle2').append('\n\t\t\t' + blogArray[1].title + '\n\t\t');
+		function callBlogAPI() {
+			$.ajax({
+				method: 'GET',
+				headers: { "X_CSRF_TOKEN": "M44ASR0FL0PJH3OLJ5RC" },
+				url: 'https://tiyagencyweek.herokuapp.com/blogs/',
+				success: function success(response) {
+					displayBlogs(response);
+				}
+			});
+		}
 
-		$('#blogText2').append('\n\t\t\t' + blogArray[1].text + '\n\t\t');
+		function expandPost() {
+			$('article').readmore({
+				speed: 60,
+				collaspedHeight: 200,
+				embedCSS: true,
+				moreLink: '<a class="readMore" href="#">Read more</a>',
+				lessLink: '<a class="readMore" href="#">Close</a>'
+			});
+		}
 
-		$('#blogTitle3').append('\n\t\t\t' + blogArray[2].title + '\n\t\t');
+		function displayBlogs(data) {
+			var $blogContainer = $('.blogHere');
+			var blogs = data.blogs;
+			var blogElement = "";
 
-		$('#blogText3').append('\n\t\t\t' + blogArray[2].text + '\n\t\t');
-	}
+			var recentThree = _.take(blogs, 3);
+
+			recentThree.forEach(function (blog) {
+				var blogPostTime = moment(blog.posted).format('MMMM DD, YYYY');
+
+				blogElement += "\n\t\t\t\t<div class=\"blog\">\n\t\t\t\t\t<article>\n\t\t\t\t\t\t<h3>" + blog.title + "</h2>\n\t\t\t\t\t\t<h4 class=\"about\">By Jake Boyles - Posted on " + blogPostTime + "</h4>\n\t\t\t\t\t\t<h4>" + blog.description + "</h4>\n\t\t\t\t\t</article>\n\t\t\t\t</div>\n\t\t\t\t";
+			});
+
+			$blogContainer.html(blogElement);
+			expandPost();
+
+			console.log(blogElement);
+		}
+
+		return {
+			init: init
+		};
+	}();
+
+	viewBlog.init();
 });
